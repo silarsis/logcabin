898b1b952e5db590cc5bd516afbaf84f58bb4ea4
Kevin Littlejohn <kevin@littlejohn.id.au>
2014-07-16 22:08:00 +1000

    notes on docker command support needed

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,6 @@ FROM ubuntu
 MAINTAINER Kevin Littlejohn <kevin@littlejohn.id.au>
 
 RUN apt-get install logrotate gzip
+# XXX Need to install docker client in here
 ADD cron.sh /usr/local/bin/cron.sh
 CMD /usr/local/bin/cron.sh
--- run.sh
+++ run.sh
@@ -4,6 +4,8 @@
 # Usage: `run.sh client_container_name`
 
 CLIENT=$1
-DOCKER_RUN="docker run -d --privileged -v /etc/localtime:/etc/localtime:ro -v /etc/timezone:/etc/timezone:ro"
+TIMEZONE_FLAGS="-v /etc/localtime:/etc/localtime:ro -v /etc/timezone:/etc/timezone:ro"
+DOCKER_RUN="docker run -d --privileged ${TIMEZONE_FLAGS}"
+# XXX Need to map the docker socket in here for docker commands to work
 
-$DOCKER_RUN --link $CLIENT:client --volumes-from $CLIENT 
+${DOCKER_RUN} --link ${CLIENT}:client --volumes-from ${CLIENT}
