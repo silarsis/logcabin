a13a00d0029d378d1d040242612fe7e25bc8a148
Kevin Littlejohn <kevin@littlejohn.id.au>
2014-07-17 13:58:24 +1000

    support, such as it is, for non-local docker daemons

--- run.sh
+++ run.sh
@@ -3,10 +3,15 @@
 # Run the container linked to a given other container
 # Usage: `run.sh client_container_name`
 
-#TIMEZONE_FLAGS="-v /etc/localtime:/etc/localtime:ro -v /etc/timezone:/etc/timezone:ro"
 # This is naive, but will work for most cases
-#DOCKER_IP=$(/sbin/ifconfig docker0 | grep 'inet ' | awk '{ print $2 }')
-DOCKER_IP=10.1.42.1
+[ ${DOCKER_IP+z} ] || {
+  DOCKER_IP=$(/sbin/ifconfig docker0 | grep 'inet ' | awk '{ print $2 }')
+}
+[ ${DOCKER_IP+z} ] || {
+  echo "This script cuold not determine the DOCKER_HOST to pass into the container."
+  echo "Please export DOCKER_IP and try again."
+  exit -1
+}
 DOCKER_RUN="docker run -d -e DOCKER_HOST=tcp://${DOCKER_IP}:4243 ${TIMEZONE_FLAGS}"
 
 set -x
