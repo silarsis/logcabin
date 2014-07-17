4903c7e2d12e262220b9f4fcc321870fe19d7470
Kevin Littlejohn <kevin@littlejohn.id.au>
2014-07-17 13:48:55 +1000

    optionally means optionally

--- logrotate.sh
+++ logrotate.sh
@@ -2,6 +2,8 @@
 #
 # Run log rotate based on the configuration file in /container/log
 
-cp /container/log/logrotate.conf /tmp/logrotate.conf
-sed -i 's/kill -(.*) CONTAINER/docker kill -s \1 ${CLIENT_NAME}' /tmp/logrotate.conf
-/usr/sbin/logrotate -v /tmp/logrotate.conf
+[ -e /container/log/logrotate.conf ] && {
+  cp /container/log/logrotate.conf /tmp/logrotate.conf
+  sed -i 's/kill -(.*) CONTAINER/docker kill -s \1 ${CLIENT_NAME}' /tmp/logrotate.conf
+  /usr/sbin/logrotate -v /tmp/logrotate.conf
+}
