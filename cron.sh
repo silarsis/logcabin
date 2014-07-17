10900a0dbbf9f131c1dfc9059ac36a146b3a36f2
Kevin Littlejohn <kevin@littlejohn.id.au>
2014-07-17 13:49:13 +1000

    comments in the git history

--- cron.sh
+++ cron.sh
@@ -22,12 +22,3 @@ for CID in ${IDS}; do
     fi
   done
 done
-
-# while true; do
-#   [ -e /container/logs/logrotate.conf ] && {
-#     cp /container/logs/logrotate.conf /tmp/logrotate.conf
-#     sed -i 's/kill -(.*) CONTAINER/docker kill -s \1 $CLIENT_NAME' /tmp/logrotate.conf
-#     /usr/sbin/logrotate -v /tmp/logrotate.conf
-#   }
-#   sleep 60
-# done
